7da8329ce5a18ff111ca5524d4b3bd22781237a2
jhuanlinarez <linarezjhuan721@gmail.com>
2024-08-29 08:52:06 -0300

    a

---
+++ modelos/avaliacao.py
@@ -0,0 +1,6 @@
+class Avalicao:
+    def __init__(self, cliente, nota):
+        self._cliente = cliente # Nome do cliente que fez a avaliação
+        self._nota = nota # Nota dada pelo cliente
+
+    # Método para converter o objeto Avaliacao em um dicionario para salvar em Json
